fe09acfa131e02f6ff5cdb4481f2c0f00af85a1b
Ismael Firmiano <ismaelfirmiano@id.uff.br>
2024-12-23 09:59:56 -0300

    Questão 5

--- P1/Q5 - GRAFO/Q5_ISMAEL.c
+++ P1/Q5 - GRAFO/Q5_ISMAEL.c
@@ -1,33 +1,42 @@
 #include "TG.c"
 #include <limits.h>
 
-int mdk(TG *g, int origem, int destino, int k, int *tam_real_caminho){
-  if(k<0)
+int mdk(TG *g, int origem, int destino, int k, int *tam_real_caminho) {
+  if (k < 0) 
     return INT_MAX;
-  if(origem == destino){
-    (*tam_real_caminho) = 0;
+
+  if (origem == destino) {
+    *tam_real_caminho = 0;
     return 0;
   }
-  int menor_dis = INT_MAX, dis;
-  TG * atual = TG_busca_no(g, origem);
 
-  if(!atual) return INT_MAX; // ESQUECI DE RETORNAR 0, CASO O NÓ NÃO EXISTA O NÓ!!!!
+  TG *atual = TG_busca_no(g, origem);
+  if (!atual)
+    return INT_MAX;
 
-  TVIZ * viz = atual->prim_viz;
-  
-  while (viz)
-  {
-    dis = viz->custo + mdk(g, viz->id_viz, destino, k-1, tam_real_caminho);
-    if(dis < menor_dis)
-      menor_dis = dis;
-      printf("Distancia %d %d\n", origem, dis);
-    viz = viz->prox_viz; // FALTOU PASSAR PARA O PRÓXIMO VIZINHO
+  TVIZ *viz = atual->prim_viz;
+  int menor_dis = INT_MAX;
+
+  while (viz) {
+    int tamanho_parcial = 0;
+
+    // INICIALIZO O DIS COM O RETORNO
+    int dis = mdk(g, viz->id_viz, destino, k - 1, &tamanho_parcial);
+
+    // TRATO CASO SEJA DIFERENTE DE INT_MAX
+    if (dis != INT_MAX && dis + viz->custo < menor_dis) {
+      menor_dis = dis + viz->custo;
+      *tam_real_caminho = tamanho_parcial + 1;
+    }
+
+    // FALTOU AVANÇAR O VIZINHO
+    viz = viz->prox_viz;
   }
-  
-  if(menor_dis == INT_MAX)
+
+  if (menor_dis == INT_MAX) {
     *tam_real_caminho = INT_MAX;
-  else 
-    *tam_real_caminho = (*tam_real_caminho) - k;
+  }
+
   return menor_dis;
 }
 
@@ -51,7 +60,7 @@ int main(void){
   int k;
   scanf("%d%d%d", &n, &m, &k);
 
-  int tam_real = k; // NO MEU ALGORÍTMO, O TAM_REAL INICIALIZA COM O K;
+  int tam_real = 0;
   custo = mdk(g, n, m, k, &tam_real);
   printf("no %d alcanca o no %d em %d arestas: %d (%d)\n", n, m, k, custo, tam_real);
   
